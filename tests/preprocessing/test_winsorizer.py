9feb686a2b1a4f12d268afa07ed6230723d1c933
charliewarburton <charliewarburton@hotmail.co.uk>
2024-11-17 18:59:43 +0000

    Creates winsorizer function. Created test for function which it passed

--- tests/preprocessing/test_winsorizer.py
+++ tests/preprocessing/test_winsorizer.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from ps3.preprocessing import Winsorizer
+from ps3.preprocessing._winsorizer import Winsorizer
 
 # TODO: Test your implementation of a simple Winsorizer
 @pytest.mark.parametrize(
@@ -11,4 +11,19 @@ def test_winsorizer(lower_quantile, upper_quantile):
 
     X = np.random.normal(0, 1, 1000)
 
-    assert False
+    winsorizer = Winsorizer(
+        lower_quantile=lower_quantile, upper_quantile=upper_quantile
+    )
+    # Fit calculates the bounds based on the quartiles
+    winsorizer.fit(X)
+
+    # Transform the data - clip values to the computed bounds
+    X_winsorized = winsorizer.transform(X)
+
+    # Compute expected bounds
+    lower_bound = np.quantile(X, lower_quantile)
+    upper_bound = np.quantile(X, upper_quantile)
+
+    # Assert that all values are within the expected bounds
+    assert np.all(X_winsorized >= lower_bound), "Some values are below the lower bound"
+    assert np.all(X_winsorized <= upper_bound), "Some values are above the upper bound"
